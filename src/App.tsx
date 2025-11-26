e95ac18043f656c82431770c25254cb8a8ae692e
amirkhangasimov <amirkhangasimovvl@gmail.com>
2025-11-26 02:03:11 +0100

    Displaying platform icons

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">Aside</GridItem>
+        <GridItem area="aside">aside</GridItem>
       </Show>
       <GridItem area="main">
         <GameGrid />
